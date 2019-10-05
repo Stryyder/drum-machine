df32c3ea8d74aa222e10d47bdcb802366a481bab
Toby Glaze <toby.r.glaze@gmail.com>
2019-10-04 22:01:50 -0700

    fixed import sounds

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
 import Drumpad from "./Drumpad";
+import Controls from "./Controls";
+import q from "./sounds/q.wav";
+import w from "./sounds/w.wav";
+import e from "./sounds/e.wav";
+import a from "./sounds/a.wav";
+import s from "./sounds/s.wav";
+import d from "./sounds/d.wav";
+import z from "./sounds/z.wav";
+import x from "./sounds/x.wav";
+import c from "./sounds/c.wav";
+import q2 from "./sounds/q2.wav";
+import w2 from "./sounds/w2.wav";
+import e2 from "./sounds/e2.wav";
+import a2 from "./sounds/a2.wav";
+import s2 from "./sounds/s2.wav";
+import d2 from "./sounds/d2.wav";
+import z2 from "./sounds/z2.wav";
+import x2 from "./sounds/x2.wav";
+import c2 from "./sounds/c2.wav";
 
 const data = [
-  { id: "test", letter: "Q", src: "sounds/q.wav" },
-  { id: "clap1", letter: "W", src: "sounds/w.wav" },
-  { id: "clap2", letter: "E", src: "sounds/e.wav" },
-  { id: "clap3", letter: "A", src: "sounds/a.wav" },
-  { id: "clap4", letter: "S", src: "sounds/s.wav" },
-  { id: "clap5", letter: "D", src: "sounds/d.wav" },
-  { id: "clap6", letter: "Z", src: "sounds/z.wav" },
-  { id: "clap7", letter: "X", src: "sounds/x.wav" },
-  { id: "clap8", letter: "C", src: "sounds/c.wav" }
+  { id: "test", letter: "Q", src: q, src2: q2 },
+  { id: "clap1", letter: "W", src: w, src2: w2 },
+  { id: "clap2", letter: "E", src: e, src2: e2 },
+  { id: "clap3", letter: "A", src: a, src2: a2 },
+  { id: "clap4", letter: "S", src: s, src2: s2 },
+  { id: "clap5", letter: "D", src: d, src2: d2 },
+  { id: "clap6", letter: "Z", src: z, src2: z2 },
+  { id: "clap7", letter: "X", src: x, src2: x2 },
+  { id: "clap8", letter: "C", src: c, src2: c2 }
 ];
 
 class App extends Component {
@@ -21,9 +40,15 @@ class App extends Component {
     this.state = {
       power: true,
       volume: "",
-      bank: "A",
+      bankExpand: false,
       soundPlayed: ""
     };
+
+    this.bankHandler = this.bankHandler.bind(this);
+  }
+
+  bankHandler() {
+    //return (this.state.bank === "1") ? this.setState(bank)
   }
 
   render() {
@@ -32,12 +57,18 @@ class App extends Component {
         <div id="display">
           <h1 id="branding">Drum Machine</h1>
           {data.map(d => (
-            <Drumpad key={d.id} id={d.id} letter={d.letter} src={d.src} />
+            <Drumpad
+              key={d.id}
+              id={d.id}
+              letter={d.letter}
+              src={d.src}
+              src2={d.src2}
+            />
           ))}
           ;
         </div>
         <div id="controls">
-          <pre>Power toggle pad pressed label volume slider Bank toggle</pre>
+          <Controls bank={this.bankHandler} />
         </div>
       </div>
     );
---
+++ src/Controls.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Controls extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="controls">Controls</div>;
+  }
+}
+
+export default Controls;
